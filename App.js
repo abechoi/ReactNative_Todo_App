743d46ab2eba3ad9b70a30dc8fa0025b2436f428
abechoi <abraham.choi@icloud.com>
2020-08-08 02:25:25 -0700

    Completed

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import Header from './components/Header';
 import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
 
@@ -17,16 +18,22 @@ export default function App() {
     });
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos]
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo submitHandler={ submitHandler }/>
         <View style={styles.list}>
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <TodoItem item={ item } pressHandler={pressHandler}/>
+              <TodoItem item={ item } pressHandler={ pressHandler }/>
             )}
           />
         </View>
